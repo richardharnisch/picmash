0e032aabccda9ae5620792471ca5f8fd8c7016d4
Richard <richitdd@gmail.com>
2025-06-02 13:46:11 +0200

    debug false :p

--- photomash/settings.py
+++ photomash/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-td$jf$*!v!hg$dd34551e+2x^1$jua0o689qczkh)m3-ffcak3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "django-server-kvorv.ondigitalocean.app",
